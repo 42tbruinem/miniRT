2b223953c9c5de26c4c589e868f51d03dfe6f711
Thijs Bruineman <tbruinem@f0r4s7.codam.nl>
2020-01-14 20:45:21 +0100

    keycodes for moving clean up

--- src/input/ft_input_key.c
+++ src/input/ft_input_key.c
@@ -6,11 +6,29 @@
 /*   By: tbruinem <tbruinem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/06 13:36:17 by tbruinem       #+#    #+#                */
-/*   Updated: 2020/01/14 20:39:55 by tbruinem      ########   odam.nl         */
+/*   Updated: 2020/01/14 20:44:40 by tbruinem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+int	ft_mlx_cam_move(int keycode, t_data *data)
+{
+	if (keycode == Z)
+		data->cams->prop.pivot.z -= 0.5;
+	if (keycode == X)
+		data->cams->prop.pivot.z += 0.5;
+	if (keycode == D)
+		data->cams->prop.pivot.x += 0.5;
+	if (keycode == A)
+		data->cams->prop.pivot.x -= 0.5;
+	if (keycode == W)
+		data->cams->prop.pivot.y += 0.5;
+	if (keycode == S)
+		data->cams->prop.pivot.y -= 0.5;
+	ft_mlx_render(data);
+	return (0);
+}
 
 int	ft_mlx_keypress(int keycode, void *param)
 {
@@ -30,35 +48,8 @@ int	ft_mlx_keypress(int keycode, void *param)
 		mlx_destroy_window(data->mlx.data, data->mlx.window);
 		exit(0);
 	}
-	if (keycode == Z)
-	{
-		data->cams->prop.pivot.z -= 0.5;
-		ft_mlx_render(data);
-	}
-	if (keycode == X)
-	{
-		data->cams->prop.pivot.z += 0.5;
-		ft_mlx_render(data);
-	}
-	if (keycode == D)
-	{
-		data->cams->prop.pivot.x += 0.5;
-		ft_mlx_render(data);
-	}
-	if (keycode == A)
-	{
-		data->cams->prop.pivot.x -= 0.5;
-		ft_mlx_render(data);
-	}
-	if (keycode == W)
-	{
-		data->cams->prop.pivot.y += 0.5;
-		ft_mlx_render(data);
-	}
-	if (keycode == S)
-	{
-		data->cams->prop.pivot.y -= 0.5;
-		ft_mlx_render(data);
-	}
+	if (keycode == Z || keycode == X || keycode == W ||
+		keycode == S || keycode == A || keycode == D)
+		return (ft_mlx_cam_move(keycode, data));
 	return (0);
 }
